29fbcd380c45c9e7d30c1ab06d12b7138e248592
kraeml <kraeml@sesamestreet.franken.de>
2017-01-18 13:58:15 +0100

    Running on ubuntu/xenial

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,28 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
+  # Falls vbguest-plugin bitte Guest nachladen
+  if Vagrant.has_plugin?("vagrant-vbguest") then
+    config.vbguest.auto_update = true
+  end
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network :private_network, ip: "192.168.68.21"
+  config.vm.hostname = "wordpress.rdf.loc"
+  # Falls vbguest-plugin bitte Guest nachladen
+  if Vagrant.has_plugin?("vagrant-hostupdater") then
+    config.hostsupdater.aliases = ["alias.testing.de", "alias2.somedomain.com"]
+  end
   config.vm.provision :shell, :path => "install.sh"
-  config.vm.synced_folder ".", "/var/www"
+  #config.vm.synced_folder ".", "/var/www"
+  config.vm.provider "virtualbox" do |vb|
+    # Display the VirtualBox GUI when booting the machine
+    #vb.gui = true
+
+    # Customize the amount of memory on the VM:
+    vb.memory = "1024"
+    vb.customize ["modifyvm", :id, "--cpus", 2]
+    vb.name = "wordpress"
+    vb.customize ["modifyvm", :id, "--cpuexecutioncap", "75"]
+    vb.linked_clone = true if Vagrant::VERSION =~ /^1.8/
+  end
 end
--- install.sh
+++ install.sh
@@ -1,27 +1,91 @@
-sudo apt-get update
+#!/bin/bash
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+# Based on https://peteris.rocks/blog/unattended-installation-of-wordpress-on-ubuntu-server/
+# Variables
+WP_DOMAIN="wordpress.rdf.loc"
+WP_ADMIN_USERNAME="admin"
+WP_ADMIN_PASSWORD="admin"
+WP_ADMIN_EMAIL="no@spam.org"
+WP_DB_NAME="wordpress"
+WP_DB_USERNAME="wordpress"
+WP_DB_PASSWORD="wordpress"
+WP_PATH="/var/www/html/wordpress"
+MYSQL_ROOT_PASSWORD="root"
 
-sudo apt-get install -y vim curl python-software-properties
-sudo add-apt-repository -y ppa:ondrej/php5
-sudo apt-get update
+# Install software
+# We are going to install nginx, PHP and MySQL.
+# By default, mysql-server is going to ask for the root password and we automate that with debconf-set-selections.
+echo "Install software"
+echo "mysql-server-5.7 mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+echo "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
+sudo apt install -y nginx php php-mysql php-curl php-gd mysql-server
 
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
-
-cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
-xdebug.scream=1
-xdebug.cli_color=1
-xdebug.show_local_vars=1
+# Configure MySQL
+# We are going to create a user and a database for WordPress. This database user will have full access to that database.
+echo "Configure MySQL"
+mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
+CREATE USER '$WP_DB_USERNAME'@'localhost' IDENTIFIED BY '$WP_DB_PASSWORD';
+CREATE DATABASE $WP_DB_NAME;
+GRANT ALL ON $WP_DB_NAME.* TO '$WP_DB_USERNAME'@'localhost';
 EOF
 
-sudo a2enmod rewrite
+# Configure nginx
+# We are going to stick to the convention used by Ubuntu and create a new
+# configuration file for the website at /etc/nginx/sites-available/domain.com
+# and symlink it as /etc/nginx/sites-enabled/domain.com.
+echo "Configure nginx"
+sudo mkdir -p $WP_PATH/public $WP_PATH/logs
+sudo tee /etc/nginx/sites-available/$WP_DOMAIN <<EOF
+server {
+  listen 80;
+  server_name $WP_DOMAIN www.$WP_DOMAIN;
 
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+  root $WP_PATH/public;
+  index index.php;
 
-sudo service apache2 restart
+  access_log $WP_PATH/logs/access.log;
+  error_log $WP_PATH/logs/error.log;
 
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+  location / {
+    try_files \$uri \$uri/ /index.php?\$args;
+  }
+
+  location ~ \.php\$ {
+    include snippets/fastcgi-php.conf;
+    fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+  }
+}
+EOF
+sudo ln -s /etc/nginx/sites-available/$WP_DOMAIN /etc/nginx/sites-enabled/$WP_DOMAIN
+sudo systemctl restart nginx
+
+# Install WordPress
+# Next, we fetch the latest version of the WordPress source code and unarchive it into $WP_PATH.
+echo "Install WordPress"
+sudo rm -rf $WP_PATH/public/ # !!!
+sudo mkdir -p $WP_PATH/public/
+sudo chown -R $USER $WP_PATH/public/
+cd $WP_PATH/public/
+
+wget --quiet https://wordpress.org/latest.tar.gz
+tar xf latest.tar.gz --strip-components=1
+rm latest.tar.gz
+
+mv wp-config-sample.php wp-config.php
+sed -i s/database_name_here/$WP_DB_NAME/ wp-config.php
+sed -i s/username_here/$WP_DB_USERNAME/ wp-config.php
+sed -i s/password_here/$WP_DB_PASSWORD/ wp-config.php
+echo "define('FS_METHOD', 'direct');" >> wp-config.php
+
+sudo chown -R www-data:www-data $WP_PATH/public/
+
+# Finally, let's perform the final step of the installation which is to choose
+# a username and password for the admin user.
+echo "Setup wp-user and wp-password"
+curl --silent "http://$WP_DOMAIN/wp-admin/install.php?step=2" \
+  --data-urlencode "weblog_title=$WP_DOMAIN"\
+  --data-urlencode "user_name=$WP_ADMIN_USERNAME" \
+  --data-urlencode "admin_email=$WP_ADMIN_EMAIL" \
+  --data-urlencode "admin_password=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "admin_password2=$WP_ADMIN_PASSWORD" \
+  --data-urlencode "pw_weak=1"
